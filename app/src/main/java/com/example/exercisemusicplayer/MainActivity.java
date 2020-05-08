09ae646688ec06f60cdf73515238d1fdbfeb6d36
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-08 21:39:18 +0700

    add dexter permission

--- app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.exercisemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +23,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myListViewForSongs = (ListView) findViewById(R.id.mySongListView);
+
+        runtimePermission();
     }
 
     public void runtimePermission() {
 
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
     }
 }
