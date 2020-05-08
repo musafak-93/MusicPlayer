aa33cb192bf94623b4f4120e172e0c2d3de5d4db
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-08 21:32:59 +0700

    Mendeklarasikan Listview di mainactivity.java

--- app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.exercisemusicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView myListViewForSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myListViewForSongs = (ListView) findViewById(R.id.mySongListView);
     }
 }
